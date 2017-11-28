b43d975f569715c2f7ac5dc7ac443fed3da4c0cb
ClassiCine <classicine.br@gmail.com>
2017-11-28 13:02:54 -0200

    alteração de card para table

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,5 @@
 window.onload = init;
-elemento = document.getElementById("filme");
+//elemento = document.getElementById("filme");
 
 function init() {
 	var aleatorio = Math.floor((Math.random() * 3) + 1);
@@ -10,16 +10,27 @@ function init() {
 	var firebase = new Firebase(conexao);
 	var database = firebase.ref().child('filmes');
 	
+	var table = document.getElementById("filme");
+	
 	database.on('value', (snap) => {
 		snap.forEach(function(data){
 			var val = data.val();
-			const filme = new Filme(val.nome, val.ano, val.diretor)
 			
-			console.log(filme.toString);
+			//const filme = new Filme(val.nome, val.ano, val.diretor);
+			//console.log(filme.toString);		
+			//elemento.innerHTML = elemento.innerHTML + filme.toString;
 			
-			elemento.innerHTML = elemento.innerHTML + filme.toString;
+			////$('#filme').append(filme.toString);
 			
-			//$('#filme').append(filme.toString);
+			var row = table.insertRow(0);
+			
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);	
+			var cell3 = row.insertCell(1);				
+			
+			cell1.innerHTML = val.nome;
+			cell2.innerHTML = val.ano;
+			cell3.innerHTML = val.diretor;
 		});
 	});
 }
