f9c9e2f03884d06c856f18376b9ed9bfc034f6c1
kleber99 <kleber_nunes99@hotmail.com>
2017-12-10 22:31:57 -0200

    ajustando layout

--- js/player.js
+++ js/player.js
@@ -2,7 +2,12 @@ var app = angular.module("player", []);
 var filme = JSON.parse(sessionStorage.getItem('filme'));
 
 app.controller('PlayerController', function ($scope){
-	
+	$scope.titulo = filme.nome;
+	$scope.ano = filme.ano;
+	$scope.nota = filme.nota;
+	$scope.duracao = filme.duracao;
+	$scope.diretor = filme.diretor;
+	$scope.sinopse = filme.sinopse;
 });
 
 app.controller('FramePlayer', ['$scope', '$sce', function ($scope, $sce) {
