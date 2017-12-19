84c8c2b2aae77723026a174471d76f346c0bb2e8
kleber99 <kleber_nunes99@hotmail.com>
2017-12-19 16:08:42 -0200

    alteração player de filme

--- js/player.js
+++ js/player.js
@@ -1,21 +1,28 @@
 var app = angular.module("player", []);
 var filme = JSON.parse(sessionStorage.getItem('filme'));
 
-app.controller('PlayerController', function ($scope){
+app.controller('PlayerController', PlayerController);
+
+function PlayerController($scope) {
+	if(filme.links[0].url != '') {
+		console.log(filme.links[0].url);
+		$scope.url = filme.links[0].url;
+	} else {
+		console.log(filme.links[1].url);
+		$scope.url = filme.links[1].url;
+	}
+	
 	$scope.titulo = filme.nome;
 	$scope.ano = filme.ano;
 	$scope.nota = filme.nota;
 	$scope.duracao = filme.duracao;
 	$scope.diretor = filme.diretor;
 	$scope.sinopse = filme.sinopse;
-});
+	$scope.img = filme.imagem;
+}
 
-app.controller('FramePlayer', ['$scope', '$sce', function ($scope, $sce) {
-	if(filme.links[0].url != '') {
-		console.log(filme.links[0].url);
-		$scope.url = $sce.trustAsResourceUrl(filme.links[0].url);
-	} else {
-		console.log(filme.links[1].url);
-		$scope.url = $sce.trustAsResourceUrl(filme.links[1].url);
-	}
+app.filter('trusted', ['$sce', function ($sce) {
+    return function(url) {
+        return $sce.trustAsResourceUrl(url);
+    };
 }]);
